5c9475c649f4d7a8d9d5dffb083e02e43712ad41
Daniel Eden <dan.eden@me.com>
2020-03-13 15:30:30 +0000

    Initial Commit

---
+++ Zeitgeist/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Zeitgeist
+//
+//  Created by Daniel Eden on 13/03/2020.
+//  Copyright © 2020 Daniel Eden. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
