e904e08f3313fc93fcfdf774c4212f12fd49628f
TODO <todo-actions[bot]@users.noreply.github.com>
2020-03-16 21:15:32 +0000

    Update TODO references: #7

--- ZeitgeistUITests/ZeitgeistUITests.swift
+++ ZeitgeistUITests/ZeitgeistUITests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 class ZeitgeistUITests: XCTestCase {
-  // TODO [$5e6fec71de0064000753d6f7]: Write UI tests
+  // TODO [#7]: Write UI tests
   // It seems fairly straightforward to write to E2E UI tests. It's well-documented
   // (e.g. on [Hacking With Swift](https://www.hackingwithswift.com/articles/148/xcode-ui-testing-cheat-sheet)
   // and mostly will require me to set up some mocks for ZEIT's API.
