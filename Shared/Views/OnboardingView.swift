681190c5d176700f0cc417a4972047601cff287e
Daniel Eden <dte@fb.com>
2022-08-22 10:58:09 +0100

    Use animated logo

--- Shared/Views/OnboardingView.swift
+++ Shared/Views/OnboardingView.swift
@@ -7,23 +7,64 @@
 
 import SwiftUI
 
+struct ZeitgeistLogo: View {
+	@ScaledMetric var size = 128
+	@State private var appear = false
+	
+	var body: some View {
+		ZStack {
+			AngularGradient(colors: [
+				Color(red: 0.34, green: 0, blue: 0.78),
+				Color(red: 0.11, green: 0.37, blue: 0.92),
+				Color(red: 0, green: 0.56, blue: 0.97),
+				Color(red: 0, green: 0.61, blue: 0.95),
+				Color(red: 0, green: 0.4, blue: 0.91),
+				Color(red: 0.39, green: 0.33, blue: 0.89),
+				Color(red: 0.76, green: 0.31, blue: 0.86),
+				Color(red: 1, green: 0.33, blue: 0.71),
+				Color(red: 1, green: 0.39, blue: 0.43),
+				Color(red: 1, green: 0.54, blue: 0.19),
+				Color(red: 1, green: 0.38, blue: 0.44),
+				Color(red: 0.87, green: 0.25, blue: 0.81),
+				Color(red: 0.34, green: 0, blue: 0.78),
+			], center: .center, angle: Angle(degrees: appear ? 360 : 0))
+			.scaleEffect(2)
+			.blur(radius: size * 0.1)
+			.clipShape(RoundedRectangle(cornerRadius: size * 0.25, style: .continuous))
+			.animation(.linear(duration: 30).repeatForever(autoreverses: false), value: appear)
+			.onAppear {
+				appear = true
+			}
+			Image(systemName: "triangle.fill")
+				.resizable()
+				.scaledToFit()
+				.padding()
+				.padding()
+				.foregroundColor(.white)
+				.shadow(color: .black.opacity(0.3), radius: size * 0.2, x: 0, y: size * 0.1)
+		}
+		.frame(width: size, height: size)
+		.shadow(color: .black.opacity(0.1), radius: size * 0.2, x: 0, y: size * 0.1)
+	}
+}
+
 struct OnboardingView: View {
 	@State var signInModel = SignInViewModel()
-
+	
 	var body: some View {
 		GeometryReader { geometry in
 			ScrollView {
 				VStack(spacing: 12) {
 					Spacer()
 
-					Image("appIcon")
-
+					.padding(.vertical)
+					
 					Text("Welcome to Zeitgeist")
 						.font(.largeTitle.bold())
 					Text("Zeitgeist lets you see and manage your Vercel deployments.")
 					Text("Watch builds complete, cancel or delete them, and get quick access to their URLs, logs, and commits.")
 						.padding(.bottom)
-
+					
 					Button(action: { signInModel.signIn() }) {
 						HStack {
 							Spacer()
@@ -34,11 +75,11 @@ struct OnboardingView: View {
 						.padding()
 						.padding(.horizontal)
 					}.buttonStyle(.borderedProminent)
-
+					
 					Text("To get started, sign in with your Vercel account.")
 						.font(.caption)
 						.foregroundColor(.secondary)
-
+					
 					Spacer()
 				}
 				.padding()
