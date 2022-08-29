ec2c6b5342a013ad1e9bb25a3a4d98442447ab48
Daniel Eden <dan.eden@me.com>
2022-08-29 20:07:42 +0100

    Improve new feature screen design and remove unused alignment code

--- Shared/Views/Onboarding/NewFeaturesView.swift
+++ Shared/Views/Onboarding/NewFeaturesView.swift
@@ -19,19 +19,6 @@ struct FeatureDescription: Hashable, Identifiable {
 	var iconType: IconType = .system
 }
 
-extension HorizontalAlignment {
-	private enum TheHorizontalAlignment : AlignmentID {
-		static func defaultValue(in d: ViewDimensions) -> CGFloat {
-			return d[HorizontalAlignment.center]
-		}
-	}
-	static let theHorizontalAlignment = HorizontalAlignment(TheHorizontalAlignment.self)
-}
-
-extension Alignment {
-	static let theAlignment = Alignment(horizontal: .theHorizontalAlignment, vertical: .center)
-}
-
 struct NewFeaturesView: View {
 	@ScaledMetric var width: CGFloat = 50
 	@Environment(\.presentationMode) private var presentationMode
@@ -51,52 +38,52 @@ struct NewFeaturesView: View {
 	var body: some View {
 		GeometryReader { geometry in
 			ScrollView {
-			VStack(spacing: 24) {
-				Spacer()
-				Text("What’s new in Zeitgeist")
-					.font(.largeTitle.bold())
-					.multilineTextAlignment(.center)
-				Spacer()
-				VStack(alignment: .leading, spacing: 24) {
-					ForEach(features) { feature in
-						HStack {
-							Group {
-								switch feature.iconType {
-								case .system:
-									Image(systemName: feature.iconName)
-								case .custom:
-									Image(feature.iconName)
+				VStack(spacing: 24) {
+					Spacer()
+					Text("What’s new in Zeitgeist")
+						.font(.largeTitle.bold())
+						.multilineTextAlignment(.center)
+					Spacer()
+					VStack(alignment: .leading, spacing: 24) {
+						ForEach(features) { feature in
+							HStack {
+								Group {
+									switch feature.iconType {
+									case .system:
+										Image(systemName: feature.iconName)
+									case .custom:
+										Image(feature.iconName)
+									}
 								}
-							}
-							.font(.largeTitle.weight(.light))
-							.foregroundStyle(.tint)
-							.frame(width: width)
-
-							VStack(alignment: .leading) {
-								Text(feature.heading).font(.headline)
-								Text(feature.description).foregroundStyle(.secondary)
+								.font(.largeTitle.weight(.light))
+								.foregroundStyle(.tint)
+								.frame(width: width)
+								
+								VStack(alignment: .leading) {
+									Text(feature.heading).font(.headline)
+									Text(feature.description).foregroundStyle(.secondary)
+								}
 							}
 						}
 					}
+					
+					Spacer()
+					Spacer()
+					
+					Button {
+						presentationMode.wrappedValue.dismiss()
+					} label: {
+						HStack {
+							Spacer()
+							Text("Continue").fontWeight(.semibold)
+							Spacer()
+						}
+						.padding()
+					}.buttonStyle(.borderedProminent)
 				}
-				
-				Spacer()
-				Spacer()
-				
-				Button {
-					presentationMode.wrappedValue.dismiss()
-				} label: {
-					HStack {
-						Spacer()
-						Text("Continue").fontWeight(.semibold)
-						Spacer()
-					}
-					.padding()
-				}.buttonStyle(.borderedProminent)
+				.padding()
+				.frame(minHeight: geometry.size.height)
 			}
-			.padding()
-			.frame(minHeight: geometry.size.height)
-		}
 		}
 	}
 }
