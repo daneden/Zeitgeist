7cc972d4bae3ce4fc45e0c74fe2c420a8f6ec0dc
Daniel Eden <dan.eden@me.com>
2022-09-23 08:41:25 +0100

    Register for background notifications when adding accounts (fixes #80)

---
+++ Shared/Models/VercelSession+accountCRUD.swift
@@ -0,0 +1,54 @@
+//
+//  VercelSession+accountCRUD.swift
+//  Zeitgeist
+//
+//  Created by Daniel Eden on 23/09/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension VercelSession {
+	static func addAccount(id: String, token: String) async {
+		guard id != .NullValue else { return }
+		
+		KeychainItem(account: id).wrappedValue = token
+		
+		let urlString = "https://api.vercel.com/v2/\(id.isTeam ? "teams/\(id)?teamId=\(id)" : "user")"
+		var request = URLRequest(url: URL(string: urlString)!)
+		
+		do {
+			request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+			let (data, _) = try await URLSession.shared.data(for: request)
+			let decoded = try JSONDecoder().decode(VercelAccount.self, from: data)
+			
+			DispatchQueue.main.async {
+				withAnimation {
+					if let index = Preferences.accounts.firstIndex(where: { $0.id == decoded.id }) {
+						Preferences.accounts[index] = decoded
+					} else {
+						Preferences.accounts.append(decoded)
+					}
+				}
+			}
+			
+#if os(iOS)
+			await UIApplication.shared.registerForRemoteNotifications()
+#elseif os(macOS)
+			await NSApplication.shared.registerForRemoteNotifications()
+#endif
+		} catch {
+			print("Encountered an error when adding account with ID \(id)")
+			print(error)
+		}
+	}
+	
+	static func deleteAccount(id: String) {
+		let keychain = KeychainItem(account: id)
+		
+		withAnimation {
+			Preferences.accounts.removeAll { id == $0.id }
+			keychain.wrappedValue = nil
+		}
+	}
+}
