d577d3fabfeabd989d845edcd6df07b965a6c114
Daniel Eden <dan.eden@me.com>
2023-06-19 10:27:35 +0100

    Add symbol effect for deployment indicators

--- Shared/Views/Deployments/DeploymentStateIndicator.swift
+++ Shared/Views/Deployments/DeploymentStateIndicator.swift
@@ -28,17 +28,32 @@ struct DeploymentStateIndicator: View {
 	}
 
 	var body: some View {
-		return Group {
-			if style == .normal {
-				Label(label, systemImage: iconName)
-			} else {
-				Label(label, systemImage: iconName)
-					.labelStyle(IconOnlyLabelStyle())
+		if #available(iOS 17.0, *) {
+			return Group {
+				if style == .normal {
+					Label(label, systemImage: iconName)
+				} else {
+					Label(label, systemImage: iconName)
+						.labelStyle(IconOnlyLabelStyle())
+				}
 			}
+			.foregroundStyle(color)
+			.symbolVariant(.fill)
+			.symbolRenderingMode(.hierarchical)
+			.contentTransition(.symbolEffect(.replace.downUp.byLayer))
+		} else {
+			return Group {
+				if style == .normal {
+					Label(label, systemImage: iconName)
+				} else {
+					Label(label, systemImage: iconName)
+						.labelStyle(IconOnlyLabelStyle())
+				}
+			}
+			.foregroundStyle(color)
+			.symbolVariant(.fill)
+			.symbolRenderingMode(.hierarchical)
 		}
-		.foregroundStyle(color)
-		.symbolVariant(.fill)
-		.symbolRenderingMode(.hierarchical)
 	}
 }
 
