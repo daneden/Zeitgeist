07bb0ed470b5e4f0f191d80eddbabdd436b64b42
Bradley David Bergeron <brad@bradbergeron.com>
2023-11-23 00:18:45 -0500

    Use Label inside WidgetLabel

--- ZeitgeistWidgets/Views/WidgetLabel.swift
+++ ZeitgeistWidgets/Views/WidgetLabel.swift
@@ -1,22 +1,44 @@
 //
-//  ZeitgeistWidgets.swift
-//  ZeitgeistWidgets
+//  WidgetLabel.swift
+//  Zeitgeist
 //
 //  Created by Daniel Eden on 31/05/2021.
+//  Updated by Brad Bergeron on 22/11/2023.
 //
 
 import SwiftUI
+
+// MARK: - WidgetLabel
 
 struct WidgetLabel: View {
 	var label: String
 	var iconName: String
 
 	var body: some View {
-		Text("\(Image(systemName: iconName)) \(label)", comment: "A widget label with an icon and label text")
-			.fontWeight(.medium)
-			.padding(2)
-			.padding(.horizontal, 2)
-			.background(.thickMaterial)
-			.clipShape(ContainerRelativeShape())
+		Label(label, systemImage: iconName)
+			.labelStyle(WidgetLabelStyle())
 	}
+}
+
+// MARK: - WidgetLabelStyle
+
+private struct WidgetLabelStyle: LabelStyle {
+
+	// MARK: Internal
+
+	func makeBody(configuration: Configuration) -> some View {
+		HStack(alignment: .center, spacing: 4) {
+			configuration.icon
+			configuration.title
+		}
+		.font(.system(size: 13, weight: .medium))
+		.padding(.horizontal, showsContainerBackground ? 4 : 0)
+		.padding(.vertical, showsContainerBackground ? 2 : 0)
+		.background(showsContainerBackground ? AnyShapeStyle(.thickMaterial) : AnyShapeStyle(.clear))
+		.clipShape(ContainerRelativeShape())
+	}
+
+	// MARK: Private
+
+	@Environment(\.showsWidgetContainerBackground) private var showsContainerBackground
 }
