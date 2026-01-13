f29187fd53e6d6931c36d483a415046dcfc10eaa
Daniel Eden <dan.eden@me.com>
2026-01-13 13:53:47 +0000

    Add animated effect to deployment list rows

--- Shared/Views/Deployments/DeploymentListRowView.swift
+++ Shared/Views/Deployments/DeploymentListRowView.swift
@@ -14,44 +14,46 @@ struct DeploymentListRowView: View {
 	var isCurrentProduction = false
 
 	var body: some View {
-		return Label {
-				VStack(alignment: .leading) {
-					HStack(spacing: 4) {
-						if deployment.target == .production {
-							Label("Production deployment", systemImage: "theatermasks")
-								.labelStyle(.iconOnly)
-								.foregroundStyle(isCurrentProduction ? AnyShapeStyle(.tint) : AnyShapeStyle(.secondary))
-								.symbolVariant(.fill)
-								.imageScale(.small)
-						}
-						
-						Text(deployment.project)
-					}
-					.font(.footnote.bold())
-					
-					switch deployment.deploymentCause {
-					case let .deployHook(name):
-						Text("\(Image(deployment.deploymentCause.icon!)) \(name)", comment: "Label for a deployment caused by a deploy hook ({icon} {name})")
-							.lineLimit(2)
+		Label {
+			VStack(alignment: .leading) {
+				HStack(spacing: 4) {
+					if deployment.target == .production {
+						Label("Production deployment", systemImage: "theatermasks")
+							.labelStyle(.iconOnly)
+							.foregroundStyle(.tint)
+							.symbolVariant(.fill)
 							.imageScale(.small)
-					case .promotion(_):
-						Text("\(Image(systemName: "arrow.up.circle")) Production rebuild", comment: "Label for a deployment caused by a promotion to production")
-							.lineLimit(2)
-							.imageScale(.small)
-					default:
-						Text(deployment.deploymentCause.description)
-							.lineLimit(2)
 					}
 					
-					VStack(alignment: .leading, spacing: 2) {
-						Text("\(deployment.created, style: .relative) ago", comment: "Timestamp for when a deployment was created in a deployment list row")
-							.fixedSize()
-							.foregroundStyle(.secondary)
-							.font(.caption)
-					}
+					Text(deployment.project)
 				}
-			} icon: {
+				.font(.footnote.bold())
+				
+				switch deployment.deploymentCause {
+				case let .deployHook(name):
+					Text("\(Image(deployment.deploymentCause.icon!)) \(name)", comment: "Label for a deployment caused by a deploy hook ({icon} {name})")
+						.lineLimit(2)
+						.imageScale(.small)
+				case .promotion(_):
+					Text("\(Image(systemName: "arrow.up.circle")) Production rebuild", comment: "Label for a deployment caused by a promotion to production")
+						.lineLimit(2)
+						.imageScale(.small)
+				default:
+					Text(deployment.deploymentCause.description)
+						.lineLimit(2)
+				}
+				
+				VStack(alignment: .leading, spacing: 2) {
+					Text("\(deployment.created, style: .relative) ago", comment: "Timestamp for when a deployment was created in a deployment list row")
+						.fixedSize()
+						.foregroundStyle(.secondary)
+						.font(.caption)
+				}
+			}
+		} icon: {
 			DeploymentStateIndicator(state: deployment.state, style: .compact)
 		}
+		.frame(maxWidth: .infinity, alignment: .leading)
+		.overlay(DeploymentStateProgressAnimation(state: deployment.state))
 	}
 }
