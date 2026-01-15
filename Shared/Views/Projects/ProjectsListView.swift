e82dc62ab33ab4b875f0eaa3448eceebf4c9f905
Daniel Eden <dan.eden@me.com>
2026-01-15 17:27:13 +0000

    Fix build error

--- Shared/Views/Projects/ProjectsListView.swift
+++ Shared/Views/Projects/ProjectsListView.swift
@@ -148,9 +148,8 @@ struct ProjectsListView: View {
 		.navigationTitle("Projects")
 		.modify {
 			if #available(iOS 26, macOS 11, *) {
-				if let account = session?.account,
-					let name = account.name ?? account.username {
-					$0.navigationSubtitle(name)
+				if let account = session?.account {
+					$0.navigationSubtitle(account.name ?? account.username)
 				} else {
 					$0
 				}
