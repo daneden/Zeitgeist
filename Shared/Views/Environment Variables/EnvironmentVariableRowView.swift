faa3af2c608370fa8d039d6f6c404e159311e30e
Daniel Eden <dan.eden@me.com>
2026-01-19 12:30:04 +0000

    Improve environment variable management experience with consolidated cross-platform views

--- Shared/Views/Environment Variables/EnvironmentVariableRowView.swift
+++ Shared/Views/Environment Variables/EnvironmentVariableRowView.swift
@@ -10,14 +10,7 @@ import SwiftUI
 struct EnvironmentVariableRowView: View {
 	@Environment(\.session) private var session
 	var projectId: VercelProject.ID
-	@State var envVar: VercelEnv
-	@State private var loading = false
-	@State private var confirmDeletion = false
-	@State private var editing = false
-	
-	var needsDecrypting: Bool {
-		envVar.decrypted == false && envVar.type == .encrypted
-	}
+	var envVar: VercelEnv
 	
 	var body: some View {
 		VStack(alignment: .leading, spacing: 4) {
@@ -33,147 +26,14 @@ struct EnvironmentVariableRowView: View {
 					.foregroundStyle(.secondary)
 			}
 			
-			HStack(spacing: 4) {
-				if envVar.type == .secret {
-					Image(systemName: "lock")
-				}
-				
-				Text(verbatim: needsDecrypting ? Array(repeating: "•", count: 15).joined() : envVar.value)
-					.privacySensitive(!needsDecrypting)
-					.contentTransition(.numericText())
-					.animation(.default, value: envVar.value)
-				
-				if loading {
-					ProgressView()
-						.controlSize(.mini)
-				}
-			}
-			.font(.footnote.monospaced())
-			.foregroundStyle(.secondary)
+			EnvironmentVariableDecryptingView(projectId: projectId, envVar: envVar)
 			
 			Text(LocalizedStringKey(envVar.target.map { $0.capitalized }.formatted(.list(type: .and))), comment: "A list of target environments for an environment variable")
 				.font(.caption)
 				.foregroundStyle(.tertiary)
 				.textSelection(.disabled)
 		}
-		.sheet(isPresented: $editing) {
-			NavigationStack {
-				EnvironmentVariableEditView(projectId: projectId,
-																		id: envVar.id,
-																		key: envVar.key,
-																		value: envVar.value,
-																		targetProduction: envVar.targetsProduction,
-																		targetPreview: envVar.targetsPreview,
-																		targetDevelopment: envVar.targetsDevelopment)
-			}
-		}
-		.onTapGesture {
-			if needsDecrypting {
-				Task {
-					loading = true
-					if let newValue = await decryptedValue() {
-						envVar = newValue
-					}
-					loading = false
-				}
-			}
-		}
-		.textSelection(.enabled)
-		.contextMenu {
-			Button {
-				Task {
-					if needsDecrypting == false {
-						Pasteboard.setString("\(envVar.key)=\(envVar.value)")
-					} else {
-						loading = true
-						
-						if let newValue = await decryptedValue() {
-							Pasteboard.setString("\(newValue.key)=\(newValue.value)")
-						}
-						loading = false
-					}
-				}
-			} label: {
-				Label {
-					HStack {
-						Text("Copy")
-						
-						if loading {
-							Spacer()
-							ProgressView()
-						}
-					}
-				} icon: {
-					Image(systemName: "doc.on.doc")
-				}
-			}
-			
-			Button {
-				if needsDecrypting == false {
-					editing = true
-				} else {
-					Task {
-						if let newValue = await decryptedValue() {
-							envVar = newValue
-							editing = true
-						}
-					}
-				}
-			} label: {
-				Label("Edit", systemImage: "pencil")
-			}
-			
-			Button(role: .destructive) {
-				confirmDeletion = true
-			} label: {
-				Label("Delete", systemImage: "trash")
-			}
-		}
-		.confirmationDialog("Delete environment variable", isPresented: $confirmDeletion) {
-			Button(role: .cancel) {
-				confirmDeletion = false
-			} label: {
-				Text("Cancel")
-			}
-			
-			Button(role: .destructive) {
-				Task {
-					await delete()
-					DataTaskModifier.postNotification()
-				}
-			} label: {
-				Text("Delete")
-			}
-		} message: {
-			Text("Are you sure you want to permanently delete this environment variable?")
-		}
-	}
-	
-	func decryptedValue() async -> VercelEnv? {
-		guard let session else { return nil }
-		do {
-			return try await EnvironmentVariableService.fetchDecrypted(
-				projectId: projectId,
-				envVarId: envVar.id,
-				session: session
-			)
-		} catch {
-			print(error)
-			return nil
-		}
-	}
-
-	func delete() async {
-		guard let session else { return }
-		do {
-			try await EnvironmentVariableService.delete(
-				projectId: projectId,
-				envVarId: envVar.id,
-				session: session
-			)
-		} catch {
-			print(error)
-		}
+		.frame(maxWidth: .infinity, alignment: .leading)
 	}
 }
 
